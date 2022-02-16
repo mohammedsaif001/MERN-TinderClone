aae47c3d8d6511e98a73427cc315158e63018ddb
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-16 22:01:13 +0530

    Header Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import "./Header.css"
+import './Header.css'
+
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+
 function Header() {
-  return (
-    <div className='header'>
-        <h1>This is a Header</h1>
-    </div>
-  )
+    return (
+        <div className='header'>
+            <IconButton>
+                <PersonIcon fontSize='large' className='header_icon' />
+            </IconButton>
+            <img src="https://www.pngitem.com/pimgs/m/33-333622_tinder-icon-tinder-icon-png-transparent-png.png" alt="TinderLogo" className="header_logo" />
+            <IconButton>
+                <ForumIcon fontSize='large' className='header_icon'/>
+            </IconButton>
+
+
+        </div>
+    )
 }
 
 export default Header
