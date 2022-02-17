ed4bde3032c260252e6385b8ebc36ea5fbbf7b18
Mohammed Saif <mdsaif4online@gmail.com>
2022-02-17 22:06:58 +0530

    Connecting to database but front end not working

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,53 @@
-const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import cards from './dbCards.js'
+import Cors from 'cors'
+
 
 //App Confing
 const app = express()
 const port = 8080
+const connection_url = `mongodb+srv://saif:saif@cluster0.vzm4p.mongodb.net/tinder?retryWrites=true&w=majority`
 
 //MiddleWares
-
-
+app.use(express.json())
+app.use(Cors())
 
 //DB Config
+mongoose.connect(connection_url);
 
 
 //API EndPoints
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+    res.send('Hello bb World!')
+})
+
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body
+    cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/card', (req, res) => {
+    const dbCard = req.body
+    cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(201).send(data)
+        }
+    })
+})
 
 
 //Listener
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
